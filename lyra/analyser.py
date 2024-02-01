de5400f2576342798562c6ebac622febfd9bb610
8baller <8ball030@gmail.com>
2024-02-01 09:53:59 +0000

    [refactor] remoed useless code

--- lyra/analyser.py
+++ lyra/analyser.py
@@ -1,52 +1,5 @@
 """
-Clas based analyser for portfolios.
-
-
-    subaccount = client.fetch_subaccount(subaccount_id=subaccount_id)
-    print(subaccount)
-
-    df = pd.DataFrame.from_records(subaccount["collaterals"])
-
-    positions = subaccount["positions"]
-
-    df = pd.DataFrame.from_records(positions)
-    df["amount"] = pd.to_numeric(df["amount"])
-    delta_columns = ['delta', 'gamma', 'vega', 'theta']
-    for col in delta_columns:
-        df[col] = pd.to_numeric(df[col])
-    if columns:
-        columns = columns.split(",")
-        df = df[[c for c in columns if c not in delta_columns] + delta_columns]
-    print("Positions")
-    open_positions = df[df['amount'] != 0]
-    open_positions = open_positions[open_positions['instrument_name'].str.contains(underlying_currency.upper())]
-    print("Greeks")
-    for col in delta_columns:
-        position_adjustment = open_positions[col] * df.amount
-        open_positions[col] = position_adjustment
-    print("Open positions")
-
-    pd.options.display.float_format = "{:,.2f}".format
-
-    open_positions = open_positions.sort_values(by=['instrument_name'])
-    if columns:
-        for col in columns:
-            try:
-                col = pd.to_numeric(open_positions[col])
-            except:
-                pass
-        print(open_positions[columns])
-    else:
-        print(open_positions)
-
-    # total deltas
-    print("Total deltas")
-    print(open_positions[delta_columns].sum())
-
-    print("Subaccount values")
-    print(subaccount['subaccount_value'])
-
-
+Class based analyser for portfolios.
 """
 
 from typing import List, Optional
